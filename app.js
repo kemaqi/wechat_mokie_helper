8ad22308e6021b0ab3d97a1106e41e0022345f94
maqi ke <you@example.com>
2019-11-16 02:21:59 +0800

    add auth pages

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
+//app.js
 App({
-  onLaunch(options) {
-    // Do something initial when launch.
-  },
-  onShow(options) {
-    // Do something when show.
-  },
-  onHide() {
-    // Do something when hide.
-  },
-  onError(msg) {
-    console.log(msg)
+  onLaunch: function () {
+    
   }
 
 })
